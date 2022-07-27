881ba51c2fb23ca949bd981a91c0b297536ab0ca
jjininu@gmail.com <jjininu@gmail.com>
2022-07-27 11:58:59 -0400

    update model_trainer

--- weekly_sales/component/model_trainer.py
+++ weekly_sales/component/model_trainer.py
@@ -1,5 +1,5 @@
 
-from weekly_sales.exception import HousingException
+from weekly_sales.exception import CustomException
 import sys
 from weekly_sales.logger import logging
 from typing import List
@@ -92,9 +92,9 @@ class ModelTrainer:
 
 
             trained_model_file_path=self.model_trainer_config.trained_model_file_path
-            housing_model = HousingEstimatorModel(preprocessing_object=preprocessing_obj,trained_model_object=model_object)
+            sales_model = SalesEstimatorModel(preprocessing_object=preprocessing_obj,trained_model_object=model_object)
             logging.info(f"Saving model at path: {trained_model_file_path}")
-            save_object(file_path=trained_model_file_path,obj=housing_model)
+            save_object(file_path=trained_model_file_path,obj=sales_model)
 
 
             model_trainer_artifact=  ModelTrainerArtifact(is_trained=True,message="Model Trained successfully",
