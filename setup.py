4e44221fa380a5768eb124bce11731645af045cf
jjininu@gmail.com <jjininu@gmail.com>
2022-07-27 20:17:23 -0400

    commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from typing import List
 
 #Declaring variables for setup functions
 PROJECT_NAME="Store-sales-predictor"
-VERSION="0.0.3"
+VERSION="0.0.4"
 AUTHOR="Junior Eshetu"
 DESRCIPTION="Machine Learning Project"
 
